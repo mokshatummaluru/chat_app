84d8c05356fde5043b4784a3afa7bd61f86ef4f9
MokshaTummaluru <112241855+mokshatummaluru@users.noreply.github.com>
2026-03-05 21:50:54 +0530

    Updated asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -1,14 +1,17 @@
 import os
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.local")
 
-from django.core.asgi import get_asgi_application
-django_asgi_app = get_asgi_application()
+django.setup()
 
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from .jwt_auth_middleware import JWTAuthMiddleware
+from core.jwt_auth_middleware import JWTAuthMiddleware
 import chat.routing
+
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
