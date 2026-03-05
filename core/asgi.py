f5d13402b3cdce76703c3b25987047864e554d30
MokshaTummaluru <112241855+mokshatummaluru@users.noreply.github.com>
2026-03-05 21:48:58 +0530

    changes asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -1,22 +1,14 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
-"""
-
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.local")
+
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
 from .jwt_auth_middleware import JWTAuthMiddleware
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
-django_asgi_app = get_asgi_application()
+import chat.routing
 
 application = ProtocolTypeRouter(
     {
