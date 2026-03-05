839ebc40a81ef1edf266c583e6adb261cd4736a9
MokshaTummaluru <112241855+mokshatummaluru@users.noreply.github.com>
2026-03-05 19:36:04 +0530

    Implemented swagger UI

--- core/urls.py
+++ core/urls.py
@@ -19,9 +19,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static 
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/',include('accounts.urls')),
     path('api/v1/',include('chat.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
